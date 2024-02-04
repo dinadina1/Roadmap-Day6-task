4fb1efb338141de499252060ab33ab1d2895c5cb
Dinakaran-FSD-Developer <123656660+dinadina1@users.noreply.github.com>
2024-02-04 17:41:48 +0530

    Oop class

--- script1.js
+++ script1.js
@@ -1,4 +1,4 @@
-// Question 2: To find Circle Area and Circumference using class
+// Question 2: To find Circle Area and Circumference using class and change color, radius also.
 
 class Circle{
     constructor(radius,color){
@@ -6,31 +6,32 @@ class Circle{
         this.color=color;
     }
     get(){
-        var str=this.radius.toString();
-        var col=this.color.toString();
-        // var radius=str.toString();
+        let str=this.radius.toString();
+        let col=this.color.toString();
         return `Circle radius is ${str} and color is ${col}.`
     }
-    set changeradius(modify){
-        this.radius=modify;
+    set changeRadius(modify){
+        this.radius=modify.toString();
     }
     set changecolor(modify1){
-        this.color=modify1;
+        this.color=modify1.toString();
     }
     getArea(){  
-        var area=3.14*(this.radius*this.radius);
+        const area=3.14 * (this.radius*this.radius);
         return `The Area of Circle is ${area}`;
     }
-    getcircum(){    
-        var circum=2*3.14*(this.radius);
-        return `The Circumference of Circle is ${circum}`
+    getCircumference(){    
+        const circumference=2 * 3.14 * (this.radius);
+        return `The Circumference of Circle is ${circumference}`
     }
-    
 }
+
 var result=new Circle(1.0,"Red");
 console.log(`Old ${result.get()}`);
-result.changeradius=2.0;
+
+result.changeRadius=2.0;
 result.changecolor="Blue";
+
 console.log(`Modified ${result.get()}`);
 console.log(result.getArea());
-console.log(result.getcircum());
+console.log(result.getCircumference());
